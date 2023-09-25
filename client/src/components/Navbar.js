d056b7e9b11c9fa2a13ebb09e057023cf6fa4da6
Jay Lim <jlimoman@gmail.com>
2023-09-25 17:57:44 -0600

    complete initial navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,20 +4,33 @@ import './Navbar.css';
 
 function Navbar() {
   return (
-    <header id="header">
-      <div class="logo">
-        <a href="../App.js"><img src="../../public/logo.png" alt="company-logo"></img></a>
-      </div>
-      <nav id="navbar">
-        <ul id="navlink">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Volunteer</a></li>
-          <li><a href="#">Audition</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Donate</a></li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="header">
+        <div class="logo">
+          <a href="../App.js"><img src="../../public/logo.png" alt="company-logo"></img></a>
+        </div>
+        <nav id="navbar">
+          <ul id="navlink">
+            <li>
+              <a href="../App.js">Home</a>
+            </li>
+            <li>
+              <a href="#">Volunteer</a>
+            </li>
+            <li>
+              <a href="#">Audition</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Donate</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div id="header-separator"></div>  
+    </>
   )
 }
 
