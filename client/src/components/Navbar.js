60fa23d2ba593ea6c617d51801e09f765f86b732
Jay Lim <jlimoman@gmail.com>
2023-09-25 16:51:45 -0600

    organize file structure and initial navbar code

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <header id="header">
+      <nav id="navbar">
+        
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar;
