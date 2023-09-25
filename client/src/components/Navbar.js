500f7891ea9f829f32eb2238003060f92d623697
Jay Lim <jlimoman@gmail.com>
2023-09-25 16:55:02 -0600

    add nav links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,13 @@ function Navbar() {
   return (
     <header id="header">
       <nav id="navbar">
-        
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Volunteer</a></li>
+          <li><a href="#">Audition</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Donate</a></li>
+        </ul>
       </nav>
     </header>
   )
