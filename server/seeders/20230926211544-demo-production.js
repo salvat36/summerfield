cf925999ec33eb7c561c72a11cc53461d82d1552
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-09-26 14:19:17 -0700

    Add production model, demo production

---
+++ server/seeders/20230926211544-demo-production.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    return queryInterface.bulkInsert('Productions', [{
+      title: 'Production 1',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('People', null, {});
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
