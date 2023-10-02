d76b04a6d4f7fd212f678d5e60d13d58028983cf
Ryan <salvat36@gmail.com>
2023-10-02 12:42:46 -0400

    add test image for main spotlight and for latest news

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,50 +5,37 @@ function Home() {
   return (
     <div id="homeContainer">
       <div id="topImageContainer">
-        {" "}
-        Image Here
         <div id="topTextLeft">
-          <h1 id="introText">Welcome To The Stage</h1>
+          <h1 id="introText"> Welcome To The Stage </h1>
         </div>
         <div id="topTextRight">
-          <h2 class='www'>Who ?</h2>
-          <span class='wwwspan'>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore.
-          </span>
-          <h2 class='www'>Why ?</h2>
-          <span class='wwwspan'>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore.
-          </span>
-          <h2 class='www'>Where ?</h2>
-          <span class='wwwspan'>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore.
-          </span>
+          <h2>Why?</h2>
+          <span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </span>
+          <h2>When?</h2>
+          <span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </span>
+          <h2>Who?</h2>
+          <span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </span>
         </div>
       </div>
       <div id="bottomContainer">
-        {" "}
-        Containere
         <div id="bottomLeft">
           <div id="latestNews">
             <h2>Latest News:</h2>
+            <p>Upcoming Auditions for XYZ Production \n 
+              Roles Available - 15</p>
+              <img id="newsImage" src="LatestNewsTestImg.jpg" alt="ADD MEANINGFUL ALT WITH DATA HERE"></img>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
           </div>
         </div>
         <div id="bottomRight">
           <div id="upcomingProductions">
             <h2>Upcoming Productions:</h2>
-
-    The Producers     Friday 7/22/23
-
-Catch me if you Can     Saturday 8/23/23
-
-Some like it hot
-Sunday 10/01/23
-
-Something Rotten
-Sunday 12/13/23
+            <h3>THE TITLE VARIABLE HERE </h3>
+            <p>DATE VARIABLE HERE </p>
+            <h3>THE TITLE VARIABLE HERE </h3>
+            <p>DATE VARIABLE HERE </p>
+            <h3>THE TITLE VARIABLE HERE </h3>
+            <p>DATE VARIABLE HERE </p>
           </div>
         </div>
       </div>
