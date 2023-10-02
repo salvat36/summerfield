78a4b012241942eaa755b0a3793af950d9586a11
Ryan <salvat36@gmail.com>
2023-10-02 14:53:10 -0400

    add buy tickets button

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,33 +9,61 @@ function Home() {
           <h1 id="introText"> Welcome To The Stage </h1>
         </div>
         <div id="topTextRight">
-          <h2 class='www'>Why?</h2>
-          <span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </span>
-          <h2 class='www'>When?</h2>
-          <span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </span>
-          <h2 class='www'>Who?</h2>
-          <span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </span>
+          <h2 class="www">Why?</h2>
+          <span>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis{" "}
+          </span>
+          <h2 class="www">When?</h2>
+          <span>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis{" "}
+          </span>
+          <h2 class="www">Who?</h2>
+          <span>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis{" "}
+          </span>
         </div>
       </div>
       <div id="bottomContainer">
         <div id="bottomLeft">
           <div id="latestNews">
             <h2>Latest News:</h2>
-            <p>Upcoming Auditions for XYZ Production \n 
-              Roles Available - 15</p>
-              <img id="newsImage" src="LatestNewsTestImg.jpg" alt="ADD MEANINGFUL ALT WITH DATA HERE"></img>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+            <p>Upcoming Auditions for XYZ Production \n Roles Available - 15</p>
+            <img
+              id="newsImage"
+              src="LatestNewsTestImg.jpg"
+              alt="ADD MEANINGFUL ALT WITH DATA HERE"
+            ></img>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis
+            </p>
           </div>
         </div>
         <div id="bottomRight">
-          <div id="upcomingProductions">
             <h2>Upcoming Productions:</h2>
-            <h3>THE TITLE VARIABLE HERE </h3>
-            <p>DATE VARIABLE HERE </p>
-            <h3>THE TITLE VARIABLE HERE </h3>
-            <p>DATE VARIABLE HERE </p>
-            <h3>THE TITLE VARIABLE HERE </h3>
-            <p>DATE VARIABLE HERE </p>
+          <div id="upcomingProductions">
+            <div class="production">
+              <h3>THE TITLE VARIABLE HERE </h3>
+              <p>DATE VARIABLE HERE </p>
+              <button class="ticketBtn">Buy Tickets</button>
+            </div>
+            <div class="production">
+              <h3>THE TITLE VARIABLE HERE </h3>
+              <p>DATE VARIABLE HERE </p>
+              <button class="ticketBtn">Buy Tickets</button>
+            </div>
+            <div class="production">
+              <h3>THE TITLE VARIABLE HERE </h3>
+              <p>DATE VARIABLE HERE </p>
+              <button class="ticketBtn">Buy Tickets</button>
+            </div>
           </div>
         </div>
       </div>
