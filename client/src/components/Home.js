354b9cb1c7d4c4e8f06ec973ab28ad8ebf584aa3
Ryan <salvat36@gmail.com>
2023-10-02 10:45:51 -0400

    setup flex layout, top/bot each split

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,13 +4,53 @@ import "./Home.css";
 function Home() {
   return (
     <div id="homeContainer">
-      <div id="topImageContainer"> Image
-        <div id="topTextLeft"> Welcome to the stage</div>
-        <div id="topTextRight">Who? Why? When?</div>
+      <div id="topImageContainer">
+        {" "}
+        Image Here
+        <div id="topTextLeft">
+          <h1 id="introText">Welcome To The Stage</h1>
+        </div>
+        <div id="topTextRight">
+          <h2 class='www'>Who ?</h2>
+          <span class='wwwspan'>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore.
+          </span>
+          <h2 class='www'>Why ?</h2>
+          <span class='wwwspan'>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore.
+          </span>
+          <h2 class='www'>Where ?</h2>
+          <span class='wwwspan'>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore.
+          </span>
+        </div>
       </div>
-      <div id="bottomContainer"> bottomContainer
-        <div id="latestNews">latestNews</div>
-        <div id="upcomingProductions">upcomingProductions</div>
+      <div id="bottomContainer">
+        {" "}
+        Containere
+        <div id="bottomLeft">
+          <div id="latestNews">
+            <h2>Latest News:</h2>
+          </div>
+        </div>
+        <div id="bottomRight">
+          <div id="upcomingProductions">
+            <h2>Upcoming Productions:</h2>
+
+    The Producers     Friday 7/22/23
+
+Catch me if you Can     Saturday 8/23/23
+
+Some like it hot
+Sunday 10/01/23
+
+Something Rotten
+Sunday 12/13/23
+          </div>
+        </div>
       </div>
     </div>
   );
