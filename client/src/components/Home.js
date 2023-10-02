c45ae7b0951d2a131911736ff102396539873d72
Ryan <salvat36@gmail.com>
2023-10-02 09:27:37 -0400

    create basic home.js structure

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div id="homeContainer">
+      <div id="topImageContainer"> Image
+        <div id="topTextLeft"> Welcome to the stage</div>
+        <div id="topTextRight">Who? Why? When?</div>
+      </div>
+      <div id="bottomContainer"> bottomContainer
+        <div id="latestNews">latestNews</div>
+        <div id="upcomingProductions">upcomingProductions</div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
