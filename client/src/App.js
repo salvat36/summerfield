aa9aa98289136f35d0040012bd445cfd832fc514
Jay Lim <jlimoman@gmail.com>
2023-10-03 13:11:56 -0600

    import BrowserRouter in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
