e7ce5fdfa400cfe25e1a7b54165e7b8d1c032b90
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-10-03 13:30:25 -0700

    Rename staff to staff members, add route

--- server/models/production.js
+++ server/models/production.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.hasMany(models.Staff, { as: 'Staff' });
+      // this.hasMany(models.StaffMember, { as: 'StaffMembers' });
     }
   }
   Production.init({
