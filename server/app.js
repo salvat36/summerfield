3d38fe27c4cf7151eaddf387bba144281d3182b3
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-10-03 11:01:53 -0700

    Update routes directory, app.js

--- server/app.js
+++ server/app.js
@@ -2,14 +2,20 @@ const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv'); 
 const app = express();
+const routes = require('./routes');
 
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const port = process.env.PORT || 3000;
 
 // Define routes & middleware
-const productionRoutes = require('./routes/productions');
-app.use('/api/v1/productions', productionRoutes);
+// const productionRoutes = require('./routes/api/productions');
+// app.use('/api/v1/productions', productionRoutes);
+
+// const userRoutes = require('./routes/users');
+// app.use('api/v1/users', userRoutes);
+
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- server/routes/productions.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productionController = require('../controllers/productionController');
-
-// Define your routes here
-router.get('/', productionController.getAllProductions);
-
-module.exports = router;
-
