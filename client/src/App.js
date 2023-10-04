462174741b414c10d8a83f37246057d0eb5f79ee
Jay Lim <jlimoman@gmail.com>
2023-10-04 07:10:39 -0600

    import react router Swtich in <App />

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import About from './components/About';
 import Home from './components/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
