4210f6f0dc1251ccc7abd550d29332c23b81943a
Jay Lim <jlimoman@gmail.com>
2023-10-04 07:17:29 -0600

    implement routes for <Home /> and <About /> in <App /> using the new react router 6 Routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,27 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import About from './components/About';
 import Home from './components/Home';
-import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/volunteer">
+          {/* Your Volunteer component */}
+        </Route>
+        <Route path="/audition">
+          {/* Your Audition component */}
+        </Route>
+        <Route path="/about" element={<About />} />
+        <Route path="/donate">
+          {/* Your Donate component */}
+        </Route>
+      </Routes>
       <Footer/>
-    </>
+    </Router>
   );
 }
 
