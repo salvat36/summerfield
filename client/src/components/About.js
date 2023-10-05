d6a24b3422622cd564a502569b031d55ed776620
Jay Lim <jlimoman@gmail.com>
2023-10-05 07:24:00 -0600

    basic outline of <About />

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,7 +3,17 @@ import './About.css';
 
 function About() {
   return (
-    <div>About</div>
+    <>
+      <div className="col-container">
+        <h2>Bringing the Community Together</h2>
+        <h2>Our Mission</h2>
+      </div>
+      <h2>Founders</h2>
+      <div className="founders-container">
+        <h3>Amy Alvino</h3>
+        <h3>Elyse Rodriguez</h3>
+      </div>
+    </>
   )
 }
 
