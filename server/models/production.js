8d05f3c97fcdd938fe7dc6249a48c0965238db50
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-10-07 13:20:31 -0700

    Update model syntax

--- server/models/production.js
+++ server/models/production.js
@@ -1,25 +1,15 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
-  class Production extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      // this.hasMany(models.StaffMember, { as: 'StaffMembers' });
+  const Production = sequelize.define('Production', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING
     }
-  }
-  Production.init({
-    title: DataTypes.STRING,
-    description: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Production',
-  });
+  }, {});
+
   return Production;
 };
