02c3df1b50c0a880ef36f5125d44f3d4a86a0b60
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-10-07 15:20:40 -0700

    Clean up code comments in app.js

--- server/app.js
+++ server/app.js
@@ -8,13 +8,6 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const port = process.env.PORT || 3000;
 
-// Define routes & middleware
-// const productionRoutes = require('./routes/api/productions');
-// app.use('/api/v1/productions', productionRoutes);
-
-// const userRoutes = require('./routes/users');
-// app.use('api/v1/users', userRoutes);
-
 app.use(routes);
 
 app.listen(port, () => {
