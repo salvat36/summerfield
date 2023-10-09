a857c15d9e746f56a74cfe201824c3026ca9f54f
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-10-09 13:32:17 -0700

    Add showtime route

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,10 +3,12 @@ const userRoute = require('./users');
 const productionRoute = require('./productions');
 const crewRoute = require('./crewmembers');
 const featureRoute = require('./features');
+const showtimeRoute = require('./showtimes');
 
 router.use('/users', userRoute);
 router.use('/productions', productionRoute);
 router.use('/crewmembers', crewRoute);
 router.use('/features', featureRoute);
+router.use('/showtimes', showtimeRoute);
 
 module.exports = router;
